84bea28883aa1ae9621f1bd6547f34c30dd9631c
Matheus Davidson <matheusdavidson@gmail.com>
2022-12-20 17:57:17 -0300

    feat(app): dispatch appLoaded on load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { appLoaded } from './core/state/menus';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-	title = 'spa-angular-typescript-dashboard';
+    title = 'spa-angular-typescript-dashboard';
+
+    constructor(private store: Store) {}
+
+    ngOnInit() {
+        this.store.dispatch(appLoaded());
+    }
 }
