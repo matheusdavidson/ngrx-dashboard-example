044b3c0542647db098999eda352554bd53598b09
Matheus Davidson <matheusdavidson@gmail.com>
2022-12-20 17:57:38 -0300

    feat(menus): dispatch actions on add, edit or delete items

--- src/app/features/menu/add-item/add-item.component.ts
+++ src/app/features/menu/add-item/add-item.component.ts
@@ -1,44 +1,49 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { BaseMenuItem, MenusStateService } from 'src/app/core';
+import { addMenuItemFormSubmitted } from 'src/app/core/state/menus';
+import { Store } from '@ngrx/store';
 
 const MenuItemPlaceholder: BaseMenuItem = {
-	name: 'French Fries',
-	price: 299,
-	tagline: 'Crispy goodness',
-	description:
-		'A plate of light and crispy French fries using Idaho potatoes and peanut oil',
-	image:
-		'https://as2.ftcdn.net/jpg/02/13/18/09/500_F_213180964_DfqvRIHj0D3t9duYUROXuQ011AgVJIaM.jpg',
-	calories: 410,
-	category: 'sides'
+    name: 'French Fries',
+    price: 299,
+    tagline: 'Crispy goodness',
+    description: 'A plate of light and crispy French fries using Idaho potatoes and peanut oil',
+    image: 'https://as2.ftcdn.net/jpg/02/13/18/09/500_F_213180964_DfqvRIHj0D3t9duYUROXuQ011AgVJIaM.jpg',
+    calories: 410,
+    category: 'sides',
 };
 
 @Component({
-	selector: 'app-add-item',
-	templateUrl: './add-item.component.html',
-	styles: [
-		`
-			:host {
-				width: 100%;
-				height: 100%;
-			}
-		`
-	]
+    selector: 'app-add-item',
+    templateUrl: './add-item.component.html',
+    styles: [
+        `
+            :host {
+                width: 100%;
+                height: 100%;
+            }
+        `,
+    ],
 })
 export class AddItemComponent {
-	menuItem = MenuItemPlaceholder;
-	constructor(
-		private location: Location,
-		private menusStateService: MenusStateService
-	) {}
+    menuItem = MenuItemPlaceholder;
+    constructor(private location: Location, private menusStateService: MenusStateService, private store: Store) {}
 
-	submit(menu: BaseMenuItem): void {
-		this.menusStateService.addMenuItem(menu);
-		this.location.back();
-	}
+    // submit(menu: BaseMenuItem): void {
+    // 	this.menusStateService.addMenuItem(menu);
+    // 	this.location.back();
+    // }
 
-	cancel(): void {
-		this.location.back();
-	}
+    submit(menu: BaseMenuItem): void {
+        this.store.dispatch(
+            addMenuItemFormSubmitted({
+                menuItem: menu,
+            })
+        );
+    }
+
+    cancel(): void {
+        this.location.back();
+    }
 }
--- src/app/features/menu/delete-item/delete-item.component.ts
+++ src/app/features/menu/delete-item/delete-item.component.ts
@@ -3,44 +3,42 @@ import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, switchMap } from 'rxjs/operators';
 import { MenusStateService } from 'src/app/core';
+import { Store } from '@ngrx/store';
+import { deleteMenuItemInitiated } from 'src/app/core/state/menus';
 
 @Component({
-  selector: 'app-delete-item',
-  templateUrl: './delete-item.component.html',
-  styleUrls: ['./delete-item.component.scss'],
+    selector: 'app-delete-item',
+    templateUrl: './delete-item.component.html',
+    styleUrls: ['./delete-item.component.scss'],
 })
 export class DeleteItemComponent {
-  menuItemId$ = this.activatedRoute.params.pipe(map((params) => params.id));
+    menuItemId$ = this.activatedRoute.params.pipe(map((params) => params.id));
+    menuItem$ = this.menuItemId$.pipe(switchMap((id) => this.menusStateService.selectMenuItem$(id)));
 
-  menuItem$ = this.menuItemId$.pipe(
-    switchMap((id) => this.menusStateService.selectMenuItem$(id))
-  );
+    constructor(private activatedRoute: ActivatedRoute, private location: Location, private router: Router, private menusStateService: MenusStateService, private store: Store) {
+        // TODO: this is a workaround, once the API is wired up
+        // we will need a way to emit latest from state service
+        this.menusStateService.fetchMenuItems();
+    }
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private location: Location,
-    private router: Router,
-    private menusStateService: MenusStateService
-  ) {
-    // TODO: this is a workaround, once the API is wired up
-    // we will need a way to emit latest from state service
-    this.menusStateService.fetchMenuItems();
-  }
+    // deleteMenuItem(id: string): void {
+    //     this.menusStateService.deleteMenuItem(id);
+    //     this.navigateHome();
+    // }
 
-  deleteMenuItem(id: string): void {
-    this.menusStateService.deleteMenuItem(id);
-    this.navigateHome();
-  }
+    deleteMenuItem(id: string): void {
+        this.store.dispatch(deleteMenuItemInitiated({ menuId: id }));
+    }
 
-  cancel(): void {
-    this.back();
-  }
+    cancel(): void {
+        this.back();
+    }
 
-  back(): void {
-    this.location.back();
-  }
+    back(): void {
+        this.location.back();
+    }
 
-  navigateHome(): void {
-    this.router.navigate(['/menu']);
-  }
+    navigateHome(): void {
+        this.router.navigate(['/menu']);
+    }
 }
